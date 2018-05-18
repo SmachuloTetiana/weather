0a4253ec76b2e08715c479ddf3cd71fbcbc74c23
SmachuloTetiana <tanichk@ukr.net>
2018-05-18 10:34:40 +0300

    weekend commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { Weather } from './shared/weather.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	loadedPage = 'weather-list';
+	title = 'app';
+
+	newSelect(page: string) {
+		this.loadedPage = page;
+	}
+
+	weathers: Weather[] = [
+		new Weather('Austria', 20),
+		new Weather('Poland', 10)
+	];
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+	@Output() pageSelected = new EventEmitter<string>();
+
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	onSelect(page: string) {
+		this.pageSelected.emit(page);
+	}
+
+}
